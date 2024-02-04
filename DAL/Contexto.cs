23bf3945d1ca99ef08530f2a98a57c2d7dd966e7
david-o-rosario1201 <finnnn023@gmail.com>
2024-02-04 00:21:57 -0400

    Creé la carpeta DAL y le agregué la clase Contexto.cs

---
+++ DAL/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroAportes.Models;
+
+namespace RegistroAportes.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Aportes> Aportes { get; set; }
+}
