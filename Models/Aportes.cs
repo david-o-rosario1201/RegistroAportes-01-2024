b1d38a5ae27dcb8a00fc2799b6c7e09ab41c0a7f
david-o-rosario1201 <finnnn023@gmail.com>
2024-02-04 00:17:08 -0400

    Creé la carpeta Models y le agregué la clase Aportes.cs

---
+++ Models/Aportes.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroAportes.Models;
+
+public class Aportes
+{
+	[Key]
+	public int AporteId { get; set; }
+
+	[Column(TypeName = "Date")]
+	public DateTime Fecha { get; set; }
+
+	[Required(ErrorMessage = "Debe ingresar un nombre")]
+	[RegularExpression(@"^[A-Za-z]+$", ErrorMessage = "El nombre no debe contener números ni carecteres especiales")]
+	public string Persona { get; set; }
+
+	[Required(ErrorMessage = "Debe ingresar una opservacion")]
+	public string Observacion { get; set; }
+
+	[Range(1, int.MaxValue, ErrorMessage = "El monto debe ser mayor a 0")]
+	public decimal Monto { get; set; }
+}
