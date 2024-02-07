3b2527f1d1eb0de427ced214604ca2cd2efb2ec3
david-o-rosario1201 <finnnn023@gmail.com>
2024-02-06 22:23:01 -0400

    Pusé que la fecha sea por defecto y corregí algunas palabras mal escritas

--- Models/Aportes.cs
+++ Models/Aportes.cs
@@ -8,14 +8,14 @@ public class Aportes
 	[Key]
 	public int AporteId { get; set; }
 
-	[Column(TypeName = "Date")]
-	public DateTime Fecha { get; set; }
+	[DataType(DataType.Date)]
+	public DateTime Fecha { get; set; } = DateTime.Now;
 
 	[Required(ErrorMessage = "Debe ingresar un nombre")]
-	[RegularExpression(@"^[A-Za-z]+$", ErrorMessage = "El nombre no debe contener números ni carecteres especiales")]
+	[RegularExpression(@"^[A-Za-z]+$", ErrorMessage = "El nombre no debe contener números ni caracteres especiales")]
 	public string Persona { get; set; }
 
-	[Required(ErrorMessage = "Debe ingresar una opservacion")]
+	[Required(ErrorMessage = "Debe ingresar una observación")]
 	public string Observacion { get; set; }
 
 	[Range(1, int.MaxValue, ErrorMessage = "El monto debe ser mayor a 0")]
